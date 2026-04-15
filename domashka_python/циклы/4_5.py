bf67bb18b0ab144f9fe739b6eeaf0925feb7853f
ishevskih <nik240408@gmail.com>
2026-04-16 00:27:21 +0300

    Циклы

--- domashka_python/циклы/4_5.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Введите количество элементов: "))
-summ = 0
-for i in range(n):
-    x = float(input(f"Введите элемент {i+1}: "))
-    summ += x ** 2  
-print("Сумма квадратов элементов =", summ)
